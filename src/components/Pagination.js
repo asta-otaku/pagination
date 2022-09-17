f60d87da8c5665287d783d6c9d33e4b7f0a8ea56
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-09-17 16:45:14 +0100

    Added Memo

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 export const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
+  const pages = useMemo(() => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  }, [totalPosts, postsPerPage]);
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
   return (
     <nav>
       <ul className="pagination">
-        {pageNumbers.map((number) => (
+        {pages.map((number) => (
           <li key={number} className="page-item">
             <a
               onClick={() => paginate(number)}
